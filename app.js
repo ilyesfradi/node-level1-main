a833a158fab0eb960b1417f0ef1069fecf5f5528
Ilyes Fradi <filyes12@gmail.com>
2023-11-27 18:56:17 +0100

    lesson 3

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html", {root: __dirname})
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
+
+mongoose.connect("mongodb+srv://ilyes_fradi:fradi123@cluster0.ifbwzck.mongodb.net/all-data?retryWrites=true&w=majority")
+.then(()=>{
+  app.listen(port, () => {
+    console.log(`http://localhost:${port}/`)
+  });
 })
+.catch((err)=> {console.log(err)});
